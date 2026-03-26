90cc4139eaafe89c0e7b3d1ecc69765c37cbe573
Owais-Khan-ICS3U1-02 <343318663@tdsb.ca>
2026-03-26 10:49:15 -0400

    Add min and isLeapYear methods to PracticeProblem
    Fix logic in isLeapYear method and add min method.

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,7 +1,23 @@
+/**
+	* Lesson: Nested Ifs
+	* Author: Owais Ali Khan
+	* Date Created: March 26, 2026
+	* Date Last Modified: March 26, 2026
+	*/
+
 public class PracticeProblem {
 
 	public static void main(String args[]) {
 
 	}
 
+	public static int min(int num1, int num2, int num3) {
+	     return Math.min(Math.min(num1, num2), num3);
+	}
+	
+	public static boolean isLeapYear(int num) {
+	     if (num%4 && (!num%100 || num%400)) {
+	         return true;
+	     return false;
+	}
 }
