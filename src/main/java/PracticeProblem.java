5ec3b1c6b5d19c08f95cf0d44e3351f59e77acde
Owais-Khan-ICS3U1-02 <343318663@tdsb.ca>
2026-03-26 11:00:32 -0400

    done

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -18,8 +18,8 @@ public class PracticeProblem {
 	public static boolean isLeapYear(int num) {
 	     if (num%4==0 && (!(num%100==0) || num%400==0)) {
 	         return true;
-	     return false;
 		 }
+		 return false;
 	}
 }
 
